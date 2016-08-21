9c0ed35a2eb206e07a39573be5952d81c8af8c24
Галина Дмитриева <gdmitrieva@lightsoft.ru>
2016-08-21 21:59:57 +0300

    Правки по игнору гита

---
+++ config/db.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=51.254.44.34;port=3306;dbname=sankras4_yii2',
+    'username' => 'sankras4_yii',
+    'password' => 'mvTHE*{jhgD57V',
+    'charset' => 'utf8',
+    'tablePrefix' => 'sk_'
+];
