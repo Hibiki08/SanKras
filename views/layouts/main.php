41098d2ce40ae0a2cdc68e0b335d1ab4a6928558
Галина Дмитриева <gdmitrieva@lightsoft.ru>
2016-08-23 15:09:44 +0300

    Правки по адаптации

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -18,7 +18,7 @@ AppAsset::register($this);
 <html>
 <head lang="en">
     <meta charset="UTF-8">
-<!--    <meta name="viewport" content="width=device-width, initial-scale=1.0">-->
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <?php echo Html::csrfMetaTags() ?>
     <title><?php echo Html::encode($this->title) ?></title>
     <?php $this->head() ?>
