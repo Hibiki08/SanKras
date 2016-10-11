e9f5fbf5a386e4fce3dbdf504036ed3ccd2676b7
Галина Дмитриева <gdmitrieva@lightsoft.ru>
2016-10-11 12:28:23 +0300

    тест письма

--- mail/discount.php
+++ mail/discount.php
@@ -5,30 +5,30 @@
     <title>Получить дисконтную карту</title>
 </head>
 <body style="width: 690px;margin: auto;">
-<div style="background: url('<?php echo Yii::$app->homeUrl; ?>/images/system/texture-mail.png') repeat;padding: 45px;">
+<div style="background: url('http://test.san-kras.ru/images/system/texture-mail.png') repeat;padding: 45px;">
     <div style="position: relative">
-        <img src="<?php echo Yii::$app->homeUrl; ?>/images/system/letter.png" style="margin-left: -38px;margin-top: -5px;">
-        <a href="<?php echo Yii::$app->homeUrl; ?>/#call-master" target="_blank" style="background: #f44336;color: #fff;text-transform: uppercase;font-size: 15px;padding: 10px 0;border-radius: 3px;border-bottom: 2px solid #cb372c;width: 200px;text-align: center;display: block;text-decoration: none;    position: absolute;bottom: 115px;left: 50px;font-family: Arial,sans-serif;">Заполнить заявку</a>
+        <img src="http://test.san-kras.ru/images/system/letter.png" style="margin-left: -38px;margin-top: -5px;">
+        <a href="http://test.san-kras.ru/#call-master" target="_blank" style="background: #f44336;color: #fff;text-transform: uppercase;font-size: 15px;padding: 10px 0;border-radius: 3px;border-bottom: 2px solid #cb372c;width: 200px;text-align: center;display: block;text-decoration: none;    position: absolute;bottom: 115px;left: 50px;font-family: Arial,sans-serif;">Заполнить заявку</a>
     </div>
     <footer style="font-family: Arial,sans-serif;">
         <ul style="float: left;line-height: 1.4;color: #9db7d1">
-            <li><a href="<?php echo Yii::$app->homeUrl; ?>/#price" target="_blank" style="color: #9db7d1;font-size: 14px;">Прайс-лист</a></li>
-            <li><a href="<?php echo Yii::$app->homeUrl; ?>/#price-flat" target="_blank" style="color: #9db7d1;font-size: 14px;">Стоимость пакетов услуг</a></li>
-            <li><a href="<?php echo Yii::$app->homeUrl; ?>/#works" target="_blank" style="color: #9db7d1;font-size: 14px;">Наши работы</a></li>
-            <li><a href="<?php echo Yii::$app->homeUrl; ?>/#reviews" target="_blank" style="color: #9db7d1;font-size: 14px;">Отзывы</a></li>
-            <li><a href="<?php echo Yii::$app->homeUrl; ?>/#contacts" target="_blank" style="color: #9db7d1;font-size: 14px;">Контакты</a></li>
+            <li><a href="http://test.san-kras.ru/#price" target="_blank" style="color: #9db7d1;font-size: 14px;">Прайс-лист</a></li>
+            <li><a href="http://test.san-kras.ru/#price-flat" target="_blank" style="color: #9db7d1;font-size: 14px;">Стоимость пакетов услуг</a></li>
+            <li><a href="http://test.san-kras.ru/#works" target="_blank" style="color: #9db7d1;font-size: 14px;">Наши работы</a></li>
+            <li><a href="http://test.san-kras.ru/#reviews" target="_blank" style="color: #9db7d1;font-size: 14px;">Отзывы</a></li>
+            <li><a href="http://test.san-kras.ru/#contacts" target="_blank" style="color: #9db7d1;font-size: 14px;">Контакты</a></li>
         </ul>
         <div style="float: right;width: 300px;line-height: 1.9;margin-top: 11px;">
             <div style="float: left">
-                <div style="position: relative;color: #9db7d1;"><img src="<?php echo Yii::$app->homeUrl; ?>/images/system/phone-letter.png" alt="Телефон" style="position: absolute;top: 6px;left: -30px;">+7 (861) 203-51-06</div>
-                <div style="position: relative;color: #9db7d1;"><img src="<?php echo Yii::$app->homeUrl; ?>/images/system/skype-letter.png" alt="skype" style="position: absolute;top: 8px;left: -30px;">skype sankras_pro</div>
-                <div style="position: relative;"><img src="<?php echo Yii::$app->homeUrl; ?>/images/system/mail-letter.png" alt="email" style="position: absolute;top: 8px;left: -30px;"><a href="mailto:info@san-kras.ru" style="color: #9db7d1;">info@san-kras.ru</a></div>
-                <div style="position: relative;color: #9db7d1;"><img src="<?php echo Yii::$app->homeUrl; ?>/images/system/work-letter.png" alt="Режим работы" style="position: absolute;top: 7px;left: -30px;">ежедневно с 8:00 до 21:00</div>
+                <div style="position: relative;color: #9db7d1;"><img src="http://test.san-kras.ru/images/system/phone-letter.png" alt="Телефон" style="position: absolute;top: 6px;left: -30px;">+7 (861) 203-51-06</div>
+                <div style="position: relative;color: #9db7d1;"><img src="http://test.san-kras.ru/images/system/skype-letter.png" alt="skype" style="position: absolute;top: 8px;left: -30px;">skype sankras_pro</div>
+                <div style="position: relative;"><img src="http://test.san-kras.ru/images/system/mail-letter.png" alt="email" style="position: absolute;top: 8px;left: -30px;"><a href="mailto:info@san-kras.ru" style="color: #9db7d1;">info@san-kras.ru</a></div>
+                <div style="position: relative;color: #9db7d1;"><img src="http://test.san-kras.ru/images/system/work-letter.png" alt="Режим работы" style="position: absolute;top: 7px;left: -30px;">ежедневно с 8:00 до 21:00</div>
             </div>
             <div style="float: right;margin-top: -9px;">
-                <a href="https://vk.com/sankras" target="_blank" style="display: block;margin-bottom: 4px;height: 40px;"><img src="<?php echo Yii::$app->homeUrl; ?>/images/system/vk.png" alt="vk"></a>
-                <a href="https://vk.com/away.php?to=https%3A%2F%2Fok.ru%2Fgroup%2F57443680583734" target="_blank" style="display: block;margin-bottom: 4px;height: 40px;"><img src="<?php echo Yii::$app->homeUrl; ?>/images/system/ok.png" alt="ok"></a>
-                <a href="https://vk.com/away.php?to=https%3A%2F%2Fwww.facebook.com%2Fgroups%2F951386194924056%2F" target="_blank" style="display: block;margin-bottom: 4px;height: 40px;"><img src="<?php echo Yii::$app->homeUrl; ?>/images/system/facebook.png" alt="facebook"></a>
+                <a href="https://vk.com/sankras" target="_blank" style="display: block;margin-bottom: 4px;height: 40px;"><img src="http://test.san-kras.ru/images/system/vk.png" alt="vk"></a>
+                <a href="https://vk.com/away.php?to=https%3A%2F%2Fok.ru%2Fgroup%2F57443680583734" target="_blank" style="display: block;margin-bottom: 4px;height: 40px;"><img src="http://test.san-kras.ru/images/system/ok.png" alt="ok"></a>
+                <a href="https://vk.com/away.php?to=https%3A%2F%2Fwww.facebook.com%2Fgroups%2F951386194924056%2F" target="_blank" style="display: block;margin-bottom: 4px;height: 40px;"><img src="http://test.san-kras.ru/images/system/facebook.png" alt="facebook"></a>
             </div>
             <div style="display: table;width: 100%;clear: both;"></div>
         </div>
