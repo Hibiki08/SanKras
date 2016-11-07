5d171a474b769bf3b7a25e10b08b857f234b03ec
Галина Дмитриева <gdmitrieva@lightsoft.ru>
2016-11-07 15:19:42 +0300

    правки

--- assets/AppAsset.php
+++ assets/AppAsset.php
@@ -10,7 +10,7 @@ class AppAsset extends AssetBundle
     public $baseUrl = '@web';
     public $css = [
         '/css/hibiki.slider.css?r3',
-        'css/style.css?r8',
+        'css/style.css?r9',
         'css/media.css?r5',
         'css/fonts.css?r3',
     ];
