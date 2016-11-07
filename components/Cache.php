d07c62cd43d6a6b69904f59c854f2526121eff94
Галина Дмитриева <gdmitrieva@lightsoft.ru>
2016-11-07 15:39:42 +0300

    правки по кэшу

--- components/Cache.php
+++ components/Cache.php
@@ -5,7 +5,7 @@ namespace app\components;
 trait Cache {
 
     public function myInit() {
-        header('Expires: Fr, 07 Nov 2016 08:57:00 GMT');
+        header('Expires: Fr, 07 Nov 2016 15:38:00 GMT');
         header('Last-Modified: ' . gmdate( 'D, d M Y H:i:s') . ' GMT');
         header('Cache-Control: no-store, no-cache, must-revalidate');
         header('Cache-Control: post-check=0, pre-check=0', false);
