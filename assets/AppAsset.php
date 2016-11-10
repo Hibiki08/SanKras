8e406acac7c3ed8ca30f0f46988297e0dff9d87d
Галина Дмитриева <gdmitrieva@lightsoft.ru>
2016-11-10 20:53:54 +0300

    высота

--- assets/AppAsset.php
+++ assets/AppAsset.php
@@ -10,7 +10,7 @@ class AppAsset extends AssetBundle
     public $baseUrl = '@web';
     public $css = [
         '/css/hibiki.slider.css?r3',
-        'css/style.css?r24',
+        'css/style.css?r25',
         'css/media.css?r25',
         'css/fonts.css?r3',
     ];
