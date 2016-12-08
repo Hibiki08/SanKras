c82ad36afa4823ba57a7fce1d7be3feb6a827d3a
Галина Дмитриева <gdmitrieva@lightsoft.ru>
2016-12-08 18:14:58 +0300

    активное меню при переходе

--- assets/OtherAsset.php
+++ assets/OtherAsset.php
@@ -13,7 +13,7 @@ class OtherAsset extends AssetBundle
 
     ];
     public $js = [
-        'js/script.js?r16',
+        'js/script.js?r17',
     ];
     public $depends = [
         'yii\web\YiiAsset',
