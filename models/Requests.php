72eaa23a40c82cd99b9a87fea7d8ae46c550d0e9
Hibiki08 <devilcatt1@gmail.com>
2017-12-24 01:30:41 +0300

    forms

--- models/Requests.php
+++ models/Requests.php
@@ -5,6 +5,21 @@ namespace app\models;
 use Yii;
 use app\components\AbstractModel;
 
+/**
+ * System model
+ *
+ * @property integer $id
+ * @property string $name
+ * @property string $phone
+ * @property string $email
+ * @property string $date
+ * @property string $type_id
+ * @property string $processe
+ * @property string $text
+ * @property string $from_page
+ */
+
+
 class Requests extends AbstractModel {
 
     const CARD_ID = 1;
