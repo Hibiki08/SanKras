fdb650442d7caa506208691e66f44951cf9af800
Hibiki08 <devilcatt1@gmail.com>
2018-03-07 19:06:26 +0300

    css

--- assets/AppAsset.php
+++ assets/AppAsset.php
@@ -12,7 +12,7 @@ class AppAsset extends AssetBundle
         '/css/hibiki.slider.css?r3',
         'css/style.css?r95',
         'css/add-style.min.css?r8',
-        'css/style.css?r101',
+        'css/style.css?r102',
         'css/media.css?r31',
         'css/fonts.css?r3',
     ];
