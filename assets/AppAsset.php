8c392313ca4dedbdcfc125caacb3ed805e28823d
Dmitrieva.GG <Dmitrieva.GG@parama.ru>
2020-04-12 18:45:51 +0300

    правки 333

--- assets/AppAsset.php
+++ assets/AppAsset.php
@@ -11,16 +11,14 @@ class AppAsset extends AssetBundle
     public $css = [
         '/css/hibiki.slider.css?r3',
         '/lib/perfect-scrollbar/css/perfect-scrollbar.css',
-        '/lib/lazy-load/dist/jquery.lazyloadxt.fadein.css',
         'css/add-style.min.css?r8',
-        'css/style.css?r104',
+        'css/style.css?r105',
         'css/media.css?r31',
         'css/fonts.css?r3',
     ];
     public $js = [
         'js/maskedinput.js?r3',
         '/js/hibiki.slider.js?r3',
-        '/lib/lazy-load/src/jquery.lazyloadxt.js',
         '/lib/lazysizes/lazysizes.min.js',
     ];
     public $depends = [
